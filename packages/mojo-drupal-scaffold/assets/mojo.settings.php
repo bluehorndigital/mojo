a82ac8c4fa0ebf8195fafebd69934fa1eaa10ae6
Matt Glaman <nmd.matt@gmail.com>
2021-08-13 15:23:39 -0500

    Move filesystems config into its own settings

--- packages/mojo-drupal-scaffold/assets/mojo.settings.php
+++ packages/mojo-drupal-scaffold/assets/mojo.settings.php
@@ -143,51 +143,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_FOR'], $_SERVER['REMOTE_ADDR'])) {
   $settings['reverse_proxy_addresses'] = $proxies;
 }
 
-if ($_ENV['FILESYSTEM_DRIVER'] === 's3') {
-  // We cannot support a private file path if using object storage.
-  unset($settings['file_private_path']);
-
-  $schemes = [
-    's3' => [
-      'driver' => 's3',
-      'config' => [
-        'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-        'region' => $_ENV['AWS_DEFAULT_REGION'],
-        'bucket' => $_ENV['S3_BUCKET'],
-        'endpoint' => $_ENV['S3_ENDPOINT'] ?? NULL,
-        'protocol' => $_ENV['S3_PROTOCOL'] ?? 'https',
-        'cname' => $_ENV['S3_CNAME'] ?? '',
-        'use_path_style_endpoint' => (bool) $_ENV['S3_USE_PATH_STYLE_ENDPOINT'],
-        'cname_is_bucket' => (bool) $_ENV['S3_CNAME_IS_BUCKET'],
-        'public' => TRUE,
-        'options' => [
-          # @todo is this needed?
-          'ACL' => 'public-read',
-        ],
-      ],
-      'cache' => TRUE,
-      // Without having this, the state value of `drupal_css_cache_files` and
-      // `system.js_cache_files` will have a mapping of non-existent files as
-      // disk storage is ephemeral in cloud environments.
-       'serve_js' => TRUE,
-       'serve_css' => TRUE,
-    ]
-  ];
-  $settings['flysystem'] = $schemes;
-  // Set the default scheme to be S3.
-  $config['system.file']['default_scheme'] = 's3';
-}
-
-if ($_ENV['FILESYSTEM_DRIVER'] !== 'local') {
-  // If the filesystem isn't local, move Twig to the temporary directory.
-  $settings['php_storage']['twig']['directory'] = $settings['file_temp_path'];
-}
-
-if (!empty($_ENV['REDIS_HOST']) && !InstallerKernel::installationAttempted() && extension_loaded('redis')) {
-  include $app_root . '/' . $site_path . '/settings.redis.php';
-}
-
 /**
  * Trusted host configuration.
  *
@@ -200,6 +155,17 @@ if (!empty($_ENV['REDIS_HOST']) && !InstallerKernel::installationAttempted() && 
  * @see default.settings.php for more information.
  */
 $settings['trusted_host_patterns'][] = '.*';
+
+// If a filesystem driver is defined and uses object storage, include config.
+if (!empty($_ENV['FILESYSTEM_DRIVER']) && $_ENV['FILESYSTEM_DRIVER'] !== 'local') {
+  include $app_root . '/' . $site_path . '/settings.filesystems.php';
+}
+
+// If a Redis host is defined, Drupal isn't installing, and the Redis extension
+// is available, include config.
+if (!empty($_ENV['REDIS_HOST']) && !InstallerKernel::installationAttempted() && extension_loaded('redis')) {
+  include $app_root . '/' . $site_path . '/settings.redis.php';
+}
 
 /**
  * Include local environment overrides.
